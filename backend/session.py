98fe9dc379ec4628cc5128a21fe60dcf185a8131
naiyoma <lankas.aurelia@gmail.com>
2023-10-12 15:08:13 +0300

    add session for test db

--- backend/session.py
+++ backend/session.py
@@ -1,9 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import settings
+from typing import Generator
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL is ",SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
