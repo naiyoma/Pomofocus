7b974b17a497f6f7975743ca7e0a298871b6f4b7
naiyoma <lankas.aurelia@gmail.com>
2023-10-25 20:26:51 +0300

    create user md

--- backend/apis/route_user.py
+++ backend/apis/route_user.py
@@ -4,7 +4,9 @@ from fastapi import Depends
 
 from schemas.user import UserCreate
 from session import get_db
-from repository.user import create_new_user
+from repository.user import create_new_user, get_all_users
+from model.models import User
+from typing import List
 
 router = APIRouter()
 
@@ -13,3 +15,7 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user,db=db)
     return user
 
+@router.get("/users/", response_model=List[User])
+def get_user(db: Session = Depends(get_db)):
+    users = get_all_users(db)
+    return users
--- backend/repository/user.py
+++ backend/repository/user.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 
 from schemas.user import UserCreate
 from model.models import User
+from typing import List
+
 
 from hashing import Hasher
 
@@ -17,3 +19,6 @@ def create_new_user(user:UserCreate, db:Session):
     db.commit()
     db.refresh(user)
     return user
+
+def get_all_users(db: Session) -> List[User]:
+    return db.query(User).all()
