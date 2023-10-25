f22baa8d274224b48049b66eb720b0c71f30214e
naiyoma <lankas.aurelia@gmail.com>
2023-10-25 22:05:44 +0300

    get all users

--- backend/apis/route_user.py
+++ backend/apis/route_user.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from sqlalchemy.orm  import Session
 from fastapi import Depends
 
-from schemas.user import UserCreate
+from schemas.user import UserCreate, ListUser
 from session import get_db
 from repository.user import create_new_user, get_all_users
 from model.models import User
@@ -15,7 +15,14 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user,db=db)
     return user
 
-# @router.get("/users/", response_model=List[User])
+# @router.get("/users/", response_model=ListUser)
 # def get_user(db: Session = Depends(get_db)):
 #     users = get_all_users(db)
+#     import pdb; pdb.set_trace()
 #     return users
+
+@router.get("/users/", response_model=List[ListUser])
+def get_user(db: Session=Depends(get_db)):
+    users = get_all_users(db)
+    list_users = [ListUser(id=user.id, email=user.email, is_active=user.is_active) for user in users]
+    return list_users
--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -5,4 +5,13 @@ class UserCreate(BaseModel):
     password : str = Field(..., min_length=4)
     confirm_password: str = Field(..., min_length=4)
     username: str = Field(..., min_length=4)
-        
+
+
+class ListUser(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+
+    class Config():
+        orm_mode = True
+
